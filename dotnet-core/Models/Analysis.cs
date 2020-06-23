7019dd5a5be57d4a4c5046672f7323558ac3f42c
Akshay Bhadange <akshaybhadange21@gmail.com>
2020-06-23 12:38:18 +0530

    Data Added

---
+++ dotnet-core/Models/Analysis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnetServer.Models
+{
+    public partial class Analysis
+    {
+        public Data()
+        {
+            Analysis = new HashSet<Analysis>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public DateTimeOffset Created { get; set; }
+
+        public virtual ICollection<Analysisdb> analysisdb_ { get; set; }
+    }
+}
---
+++ dotnet-core/Models/Databinding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD_Operations.Models
+{
+    public partial class Dataset
+    {
+        public Dataset()
+        {
+            Db = new HashSet<Db>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public int Gender { get; set; }
+        public string Specialist { get; set; }
+        public DateTimeOffset Created { get; set; }
+
+        public virtual ICollection<Db> data_ { get; set; }
+    }
+}
